432f5378c9401a2fb20f4d618df2c95ab73e1f29
annisapritama <annisaprtma08@gmail.com>
2024-05-18 14:30:05 +0700

    Tugas 8: OOP PHP

---
+++ oop/ape.php
@@ -0,0 +1,13 @@
+<?php
+
+class ape {
+    public $legs = 2;
+    public $cold_blooded = "no";
+    public $name = 'kera sakti';
+
+    public function Yell() {
+        return "Auooo";
+    }
+}
+
+?>
